0a5771a1196c9d89fb50aded1b0e33365045bbfa
farahalfawzy <100588601+farahalfawzy@users.noreply.github.com>
2022-08-03 12:54:44 +0200

    Update audios.py

--- audios.py
+++ audios.py
@@ -6,17 +6,17 @@ from itertools import zip_longest
 from matplotlib import pyplot as plt
 import sys
 
-duration_list=[]
-filename_list=[]
-samplingrate_list=[]
 
     
 
 
-def audiofiles(path,c):
+def audiofiles(path,c,bins):
     
     if(path=='.'):
         path=os.getcwd()
+    duration_list=[]
+    filename_list=[]
+    samplingrate_list=[]
  
     for filename in glob.glob(os.path.join(path, '*.wav')):
         w = wave.open(filename, 'r')
@@ -32,10 +32,10 @@ def audiofiles(path,c):
         saveAddr=os.getcwd()+'/audio-information.csv' 
     else:    
         saveAddr=c+'/audio-information.csv'   
-    generate_csv_file(saveAddr)
-    generate_histogram()
+    generate_csv_file(saveAddr,filename_list,duration_list,samplingrate_list)
+    generate_histogram(duration_list,bins)
 
-def generate_csv_file(saveAddr):
+def generate_csv_file(saveAddr,filename_list,duration_list,samplingrate_list):
     data = [filename_list,duration_list,samplingrate_list]
     export_data = zip_longest(*data, fillvalue = '')
     with open(saveAddr, 'w', encoding="ISO-8859-1", newline='') as file:
@@ -44,11 +44,14 @@ def generate_csv_file(saveAddr):
       write.writerows(export_data)
 
 
-def  generate_histogram():
-    plt. hist(duration_list)
+def generate_histogram(duration_list,bin):
+    plt. hist(duration_list,bins=bin)
     plt.xlabel("Duration")
     plt.ylabel("Frequency")
     plt.show()
-
-audiofiles(sys.argv[1],sys.argv[2])  
+    
+if __name__=='__main__':
+    audiofiles(sys.argv[1],sys.argv[2],sys.argv[3])
+   
+    
      
